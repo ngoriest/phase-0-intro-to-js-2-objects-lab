5a855efb61a245ab235ca1dde3aecce1d6e365b2
Manase Kimutai <mernasseh@gmail.com>
2025-02-27 23:14:18 +0300

    initiatwe commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// Write your solution in this file!
+let employee = {
+    name: 'Ian',
+    streetAddress: 'Kenya'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+updateEmployeeWithKeyAndValue(employee, 'name', 'Sam');
